4ffa7946b9e128798d49473dcbb00663c2672841
pharmakarios1 <akhigbeblessed@gmail.coom>
2024-06-19 15:11:42 +0100

    added the job component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,24 +2,24 @@ import { FaArrowRightToBracket } from "react-icons/fa6";
 
 const Login = () => {
   return (
-    <div className="flex gap-3 m-2 items-center justify-end">
+    <div className="flex gap-3  items-center justify-end bg-slate-300">
       <div className="flex gap-3 ">
-        <a href="#" className="no-underline text-inherit">
+        <a href="#" className="no-underline text-inherit font-bold">
           Contact Us
         </a>
         <a href="#" className="flex items-center no-underline text-inherit ">
-          <span className="mr-2">Sign In</span>
+          <span className="mr-1">Sign In</span>
           <FaArrowRightToBracket className="text-blue-500" />
         </a>
       </div>
-      <div className="flex gap-2">
-        <button className=" rounded-xl flex items-center justify-center w-10 h-6 border-1 border-blue-500 ">
+      <div className="flex gap-2 border-2">
+        <button className=" rounded-xl flex items-center justify-center mt-1 mb-1 w-10 h-6 border-1 text-[12px] font-bold border-blue-500 ">
           ENG
         </button>
-        <button className=" rounded-xl flex items-center justify-center w-10 h-6 border-1 border-blue-500">
+        <button className=" rounded-xl flex items-center justify-center mt-1 mb-1 w-10 h-6 border-1 text-[12px] font-bold border-blue-500">
           FRA
         </button>
-        <button className=" rounded-xl flex items-center justify-center w-10 h-6 border-1 border-blue-500">
+        <button className=" rounded-xl flex items-center justify-center mt-1 mb-1 w-10 h-6 border-1 text-[12px] font-bold border-blue-500">
           IND
         </button>
       </div>
