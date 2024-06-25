c99f4bb1054e658703b02cd01ed11c81f70e71f5
pharmakarios1 <akhigbeblessed@gmail.coom>
2024-06-25 15:26:43 +0100

    Added Auth page with sign in

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -6,14 +6,19 @@ const ContactUs = () => {
       <div className="bg-blue-100 min-h-[100vh] py-4 px-4 flex flex-col items-center">
         <h5>Contact us</h5>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col ">
           <div className="card">
             <div className="card-body">
-              <p className="text-slate-400">
-                Email: info@job-management.com OR akhigbeblessed@gmail.com
+              <p className="text-slate-500 text-center">
+                Email: info@job-management.com
               </p>
 
-              <p>Pharmakarios1</p>
+              <p className="text-[10px]">
+                Pharmakarios1 <br />
+                <span className="text-slate-400">akhigbeblessed@gmail.com</span>
+                <br />
+                <span className="text-slate-400">08060514714</span>
+              </p>
               <h6 className="flex items-center gap-2">
                 <CiLocationOn className="text-red-500 text-2xl" /> Lagos,
                 Nigeria
